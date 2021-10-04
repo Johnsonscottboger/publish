256d2d47755f001e34a4a5966d2487d9d95da4db
waning1995 <a937515012@live.com>
2021-10-04 11:41:48 +0800

    add upload

--- src/main/kotlin/com/zentao/publish/controller/ProjectController.kt
+++ src/main/kotlin/com/zentao/publish/controller/ProjectController.kt
@@ -6,6 +6,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.notExists
 
 @Controller
 @RequestMapping("api/project")
@@ -16,7 +21,7 @@ class ProjectController {
     private lateinit var _service: IProjectService
 
     @ResponseBody
-    @PostMapping("/create",  produces = ["application/json"])
+    @PostMapping("/create", produces = ["application/json"])
     fun create(@RequestBody project: Project): String {
         return _service.create(project)
     }
@@ -35,13 +40,29 @@ class ProjectController {
 
     @ResponseBody
     @GetMapping("/{id}")
-    fun get(@PathVariable id: String) : Project? {
+    fun get(@PathVariable id: String): Project? {
         return _service.getById(id)
     }
 
     @ResponseBody
     @GetMapping("/user/{userId}")
-    fun getByUserId(@PathVariable userId: String) : List<Project> {
+    fun getByUserId(@PathVariable userId: String): List<Project> {
         return _service.getByUserId(userId)
+    }
+
+    @ResponseBody
+    @PostMapping("/upload/{projectId}", produces = ["application/json"])
+    fun upload(@PathVariable projectId: String, @RequestParam file: MultipartFile): String {
+        if (file.isEmpty)
+            throw IllegalArgumentException("file can not be empty")
+        val fileName = file.originalFilename!!
+        val project = _service.getById(projectId) ?: throw IllegalArgumentException("找不到指定的项目")
+        val path = Path(System.getenv("appdata"), "publish", "project", project.name!!)
+        if (path.notExists())
+            path.createDirectories()
+        val dest = File(path.toString(), fileName)
+        if(dest.exists()) dest.delete()
+        file.transferTo(dest)
+        return "上传成功"
     }
 }
--- src/test/kotlin/com/zentao/publish/PublishApplicationTests.kt
+++ src/test/kotlin/com/zentao/publish/PublishApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.zentao.publish
 
+import com.zentao.publish.service.mail.IMailService
+import com.zentao.publish.viewmodel.MailSendInfo
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class PublishApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    @Autowired
+    private lateinit var _mailService: IMailService
 
+    @Test
+    fun send() {
+        val info = MailSendInfo(
+            productName = "2021D01 英思基础数据中台",
+            productPublishPath = "http://psvn.wuhanins.com/svn/产品发布库/2021D01 基础数据中台",
+            projectName = "TB水电站绿色智能建造",
+            projectVersion = "1.01.{00i}.{yyMMdd}",
+            projectPublishPath = "http://psvn.wuhanins.com/svn/2020/2020Z11 TB水电站绿色智能建造平台、通用管理系统建设及维护项目/工作库/2产品生产/2.4软件测试/2.4.4提测版本/",
+            zentaoAddress = "http://zentao.wuhanins.com:88/zentao/my/",
+            description = ""
+        )
+        _mailService.send("wangnb@wuhanins.com", info)
+    }
 }
